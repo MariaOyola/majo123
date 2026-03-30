965124bdabff10454221025886e4677c38a32b8c
Maria Jose <mariajoserodriguezoyola@gmaail.com>
2026-03-30 18:25:56 -0500

    se agrega tabla producto ejecutando correctamente

---
+++ repo-test/liquibase/changelog/09_inserts/0006_insert_product.sql
@@ -0,0 +1,9 @@
+INSERT INTO product (name, price, id_category, id_status) VALUES
+('Coca Cola', 5000, 1, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),
+('Pepsi', 4800, 1, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),
+('Hamburguesa', 12000, 2, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),
+('Pizza personal', 15000, 2, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),
+('Perro caliente', 8000, 2, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),
+('Helado', 6000, 3, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),
+('Brownie', 7000, 3, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),
+('Papas fritas', 4000, 4, '87dd1b41-9fdd-4e64-949d-c65d44a322ac');
