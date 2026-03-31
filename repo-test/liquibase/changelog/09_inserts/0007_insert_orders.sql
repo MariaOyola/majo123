003234693e813005cf81011948cfdced001fc3d9
Maria Jose <mariajoserodriguezoyola@gmaail.com>
2026-03-30 19:43:52 -0500

    proyecto de base de datos ya listo para producion

---
+++ repo-test/liquibase/changelog/09_inserts/0007_insert_orders.sql
@@ -0,0 +1,6 @@
+INSERT INTO orders (id_user, total, id_status) VALUES
+(7, 17000, '825b9d46-7082-42c0-8088-348e724ba5ea'),  -- entregado
+(8, 5000, '7f1af340-0a19-460a-9383-04cc5d6297be'),   -- pendiente
+(9, 12000, '87dd1b41-9fdd-4e64-949d-c65d44a322ac'),  -- activo
+(11, 22000, '825b9d46-7082-42c0-8088-348e724ba5ea'),  -- entregado
+(12, 8000, '9f6c6366-ab16-4047-9a5c-f07e81a9a5e4');   -- eliminado (ejemplo)
